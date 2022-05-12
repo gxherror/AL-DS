78f50a5901710ec5467ef04dcdbcca5d3d07d8a7
xherror@win <gxherror@gmail.com>
2022-05-12 20:23:49 +0800

    init storage

---
+++ DS/graph/storage.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+struct E
+{
+   char src,dst;
+   int w;
+    /* data */
+}* e;
+int main(int argc, char const *argv[])
+{
+    char* v;
+    int m=0,n=0;
+    cout<< "HELLO WORLD" << endl;
+    scanf("node num n:%d,edge num m:%d",&n,&m);
+    return 0;
+}
